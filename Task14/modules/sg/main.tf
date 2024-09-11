1b1953a319821674e4446a687bae7f92b28c6a24
Ameera <ameera.rizvi@hotmail.com>
2024-09-11 21:53:54 +0500

    Task 14

--- Task14/modules/sg/main.tf
+++ Task14/modules/sg/main.tf
@@ -40,6 +40,20 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
   egress {
     from_port   = 0
     to_port     = 0
